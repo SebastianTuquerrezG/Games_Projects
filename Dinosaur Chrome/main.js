e621972f8667447ed35fe7193146e7ad501e0b08
Sebastian TG <jtuquerrez@unicauca.edu.co>
2023-10-13 14:19:19 -0500

    Animacion del terreno

--- Dinosaur Chrome/main.js
+++ Dinosaur Chrome/main.js
@@ -20,8 +20,74 @@ function Loop(){
     requestAnimationFrame(Loop);
 }
 
+var floorY = 22;
+var valY = 0;
+var impulse = 900;
+var gravity = 2500;
 
+var dinoPosX = 45;
+var dinoPosY = floorY;
+
+var floorX = 0;
+var velEscenary = 1280/3;
+var gameVel = 1;
+var score = 0;
+
+var stop = false;
+var jump = false;
+
+var dino, container, textScore, floor, gameOver;
+
+function Start(){
+    gameOver = document.querySelector(".game-over");
+    floor = document.querySelector(".floor");
+    container = document.querySelector(".container");
+    textScore = document.querySelector(".score");
+    dino = document.querySelector(".dino");
+    document.addEventListener("keydown", HandleKeyDown);
+}
+
+function HandleKeyDown(ev){
+    if(ev.KeyCode == 32){
+        Saltar();
+    }
+}
+
+function Saltar(){
+    if(dinoPosY == floorY){
+        jump = true;
+        valY = impulse;
+        dino.classList.remove("dino-run")
+    }
+}
 
 function update(){
+    moveFloor();
+    moveDinosaur();
+}
 
+function moveFloor(){
+    floorX += calculateDesplazement();
+    floor.style.left = -(floorX % container.clientWidth) + "px";
+}
+
+function calculateDesplazement(){
+    return velEscenary * deltaTime * gameVel;
+}
+
+function moveDinosaur(){
+    dinoPosY  += gameVel * deltaTime;
+    if(dinoPosY < floorY){
+        touchFloor();
+    }
+    dino.style.bottom = dinoPosY + "px";
+}
+
+function touchFloor(){
+    dinoPosY = floorY;
+    velY = 0;
+    if(jump){
+        dino.classList.add("dino-run");
+    }
+    jump = false;
 }
