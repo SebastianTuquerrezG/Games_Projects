1557e0500950b667970f8d39359fdd790a57056c
Joan Sebastian Tuquerrez Gomez <jtuquerrez@unicauca.edu.co>
2023-10-13 21:46:22 -0500

    Dinosaur Chrome Finished

--- Dinosaur Chrome/main.js
+++ Dinosaur Chrome/main.js
@@ -1,93 +1,240 @@
+//****** GAME LOOP ********//
+
 var time = new Date();
 var deltaTime = 0;
 
 if(document.readyState === "complete" || document.readyState === "interactive"){
     setTimeout(Init, 1);
-} else{
-    document.addEventListener("DOMContentLoaded", Init)
+}else{
+    document.addEventListener("DOMContentLoaded", Init); 
 }
 
-function Init(){
+function Init() {
     time = new Date();
-    Start()
-    Loop()
+    Start();
+    Loop();
 }
 
-function Loop(){
-    deltaTime = (new Date() - time)/1000;
+function Loop() {
+    deltaTime = (new Date() - time) / 1000;
     time = new Date();
-    update();
+    Update();
     requestAnimationFrame(Loop);
 }
 
+//****** GAME LOGIC ********//
+
 var floorY = 22;
-var valY = 0;
+var velY = 0;
 var impulse = 900;
 var gravity = 2500;
 
-var dinoPosX = 45;
-var dinoPosY = floorY;
+var dinoPosX = 42;
+var dinoPosY = floorY; 
 
 var floorX = 0;
-var velEscenary = 1280/3;
+var velScenery = 1280/3;
 var gameVel = 1;
 var score = 0;
 
-var stop = false;
-var jump = false;
+var stopped = false;
+var jumping = false;
 
-var dino, container, textScore, floor, gameOver;
+var timeToObstacle = 2;
+var timeObstacleMin = 0.7;
+var timeObstacleMax = 1.8;
+var obstaclePosY = 16;
+var obstacles = [];
 
-function Start(){
+var timeToClouds = 0.5;
+var timeCloudMin = 0.7;
+var timeCloudMax = 2.7;
+var maxCloudY = 270;
+var minCloudY = 100;
+var clouds = [];
+var velCloud = 0.5;
+
+var container;
+var dino;
+var textoScore;
+var floor;
+var gameOver;
+
+function Start() {
     gameOver = document.querySelector(".game-over");
     floor = document.querySelector(".floor");
     container = document.querySelector(".container");
-    textScore = document.querySelector(".score");
+    textoScore = document.querySelector(".score");
     dino = document.querySelector(".dino");
     document.addEventListener("keydown", HandleKeyDown);
 }
 
+function Update() {
+    if(stopped) return;
+    
+    MoveDinosaur();
+    MoveFloor();
+    DecideMakeObstacles();
+    DecideMakeClouds();
+    MoveObstacles();
+    MoveClouds();
+    DetectCollision();
+
+    velY -= gravity * deltaTime;
+}
+
 function HandleKeyDown(ev){
-    if(ev.KeyCode == 32){
-        Saltar();
+    if(ev.keyCode == 32){
+        Jump();
     }
 }
 
-function Saltar(){
-    if(dinoPosY == floorY){
-        jump = true;
-        valY = impulse;
-        dino.classList.remove("dino-run")
+function Jump(){
+    if(dinoPosY === floorY){
+        jumping = true;
+        velY = impulse;
+        dino.classList.remove("dino-run");
     }
 }
 
-function update(){
-    moveFloor();
-    moveDinosaur();
+function MoveDinosaur() {
+    dinoPosY += velY * deltaTime;
+    if(dinoPosY < floorY){
+        
+        TouchFloor();
+    }
+    dino.style.bottom = dinoPosY+"px";
 }
 
-function moveFloor(){
-    floorX += calculateDesplazement();
+function TouchFloor() {
+    dinoPosY = floorY;
+    velY = 0;
+    if(jumping){
+        dino.classList.add("dino-run");
+    }
+    jumping = false;
+}
+
+function MoveFloor() {
+    floorX += CalculateDisplacement();
     floor.style.left = -(floorX % container.clientWidth) + "px";
 }
 
-function calculateDesplazement(){
-    return velEscenary * deltaTime * gameVel;
+function CalculateDisplacement() {
+    return velScenery * deltaTime * gameVel;
 }
 
-function moveDinosaur(){
-    dinoPosY  += gameVel * deltaTime;
-    if(dinoPosY < floorY){
-        touchFloor();
-    }
-    dino.style.bottom = dinoPosY + "px";
+function Crash() {
+    dino.classList.remove("dino-run");
+    dino.classList.add("dino-crash");
+    stopped = true;
 }
 
-function touchFloor(){
-    dinoPosY = floorY;
-    velY = 0;
-    if(jump){
-        dino.classList.add("dino-run");
+function DecideMakeObstacles() {
+    timeToObstacle -= deltaTime;
+    if(timeToObstacle <= 0) {
+        MakeObstacle();
     }
-    jump = false;
+}
+
+function DecideMakeClouds() {
+    timeToClouds -= deltaTime;
+    if(timeToClouds <= 0) {
+        MakeCloud();
+    }
+}
+
+function MakeObstacle() {
+    var obstacle = document.createElement("div");
+    container.appendChild(obstacle);
+    obstacle.classList.add("cactus");
+    if(Math.random() > 0.5) obstacle.classList.add("cactus2");
+    obstacle.posX = container.clientWidth;
+    obstacle.style.left = container.clientWidth+"px";
+
+    obstacles.push(obstacle);
+    timeToObstacle = timeObstacleMin + Math.random() * (timeObstacleMax-timeObstacleMin) / gameVel;
+}
+
+function MakeCloud() {
+    var cloud = document.createElement("div");
+    container.appendChild(cloud);
+    cloud.classList.add("cloud");
+    cloud.posX = container.clientWidth;
+    cloud.style.left = container.clientWidth+"px";
+    cloud.style.bottom = minCloudY + Math.random() * (maxCloudY-minCloudY)+"px";
+    
+    clouds.push(cloud);
+    timeToClouds = timeCloudMin + Math.random() * (timeCloudMax-timeCloudMin) / gameVel;
+}
+
+function MoveObstacles() {
+    for (var i = obstacles.length - 1; i >= 0; i--) {
+        if(obstacles[i].posX < -obstacles[i].clientWidth) {
+            obstacles[i].parentNode.removeChild(obstacles[i]);
+            obstacles.splice(i, 1);
+            WinPoints();
+        }else{
+            obstacles[i].posX -= CalculateDisplacement();
+            obstacles[i].style.left = obstacles[i].posX+"px";
+        }
+    }
+}
+
+function MoveClouds() {
+    for (var i = clouds.length - 1; i >= 0; i--) {
+        if(clouds[i].posX < -clouds[i].clientWidth) {
+            clouds[i].parentNode.removeChild(clouds[i]);
+            clouds.splice(i, 1);
+        }else{
+            clouds[i].posX -= CalculateDisplacement() * velCloud;
+            clouds[i].style.left = clouds[i].posX+"px";
+        }
+    }
+}
+
+function WinPoints() {
+    score++;
+    textoScore.innerText = score;
+    if(score == 5){
+        gameVel = 1.5;
+        container.classList.add("noon");
+    }else if(score == 10) {
+        gameVel = 2;
+        container.classList.add("tarde");
+    } else if(score == 20) {
+        gameVel = 3;
+        container.classList.add("night");
+    }
+    floor.style.animationDuration = (3/gameVel)+"s";
+}
+
+function GameOver() {
+    Crash();
+    gameOver.style.display = "block";
+}
+
+function DetectCollision() {
+    for (var i = 0; i < obstacles.length; i++) {
+        if(obstacles[i].posX > dinoPosX + dino.clientWidth) {
+            //EVADE
+            break;
+        }else{
+            if(IsCollision(dino, obstacles[i], 10, 30, 15, 20)) {
+                GameOver();
+            }
+        }
+    }
+}
+
+function IsCollision(a, b, paddingTop, paddingRight, paddingBottom, paddingLeft) {
+    var aRect = a.getBoundingClientRect();
+    var bRect = b.getBoundingClientRect();
+
+    return !(
+        ((aRect.top + aRect.height - paddingBottom) < (bRect.top)) ||
+        (aRect.top + paddingTop > (bRect.top + bRect.height)) ||
+        ((aRect.left + aRect.width - paddingRight) < bRect.left) ||
+        (aRect.left + paddingLeft > (bRect.left + bRect.width))
+    );
 }
